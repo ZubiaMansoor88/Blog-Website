4227c4877c3983c82935516511c349fe39327ff9
ZubiaMansoor88 <zubia.lhr@gmail.com>
2024-03-14 12:14:29 +0500

    update requirement

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # import smtplib
 import os
 
+
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
